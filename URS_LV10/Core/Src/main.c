c0c4490fe4d02da0c37b65dc7a039651d0c78132
Franko Šošić <fsosic@tvz.hr>
2024-01-15 13:06:25 +0100

    dodan blinky program

--- URS_LV10/Core/Src/main.c
+++ URS_LV10/Core/Src/main.c
@@ -93,6 +93,11 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+	  HAL_Delay(200);
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+	  HAL_Delay(200);
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
